233ca1c08b4e46259656dccc10527ebb1a15fa25
daffaya <daffapasya21@gmail.com>
2023-05-12 13:27:25 +0800

    Finishing UI for Signup Page

--- app/src/main/java/com/codegeniuses/estetikin/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/signup/SignUpActivity.kt
@@ -1,12 +1,12 @@
-package com.codegeniuses.estetikin.ui.register
+package com.codegeniuses.estetikin.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codegeniuses.estetikin.R
 
-class RegisterActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_sign_up)
     }
 }
