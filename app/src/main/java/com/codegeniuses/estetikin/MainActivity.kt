a1de9f59d6770c62b619586d3b41854ab159143f
daffaya <daffapasya21@gmail.com>
2023-05-25 22:35:22 +0700

    Navigation with custom NavBar

--- app/src/main/java/com/codegeniuses/estetikin/MainActivity.kt
+++ app/src/main/java/com/codegeniuses/estetikin/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.codegeniuses.estetikin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.commit
-import androidx.fragment.app.commitNow
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.codegeniuses.estetikin.databinding.ActivityMainBinding
-import com.codegeniuses.estetikin.ui.home.HomeFragment
 import com.codegeniuses.estetikin.ui.login.LoginActivity
 import com.codegeniuses.estetikin.ui.signup.SignUpActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+        binding.botNav.background = null
+        binding.botNav.menu.getItem(2).isEnabled = false
 
         binding.btnTestLogin.setOnClickListener{
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
@@ -33,16 +32,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-
-        val botNav: BottomNavigationView = binding.botNav
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_home_nav) as NavHostFragment
         val navController = navHostFragment.navController
+
+        val botNav: BottomNavigationView = binding.botNav
         botNav.setupWithNavController(navController)
 
     }
-
-
 }
