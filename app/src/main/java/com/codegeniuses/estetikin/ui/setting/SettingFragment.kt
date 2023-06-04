d65f77754c36e7a674d766d03d58ff7b043fe64c
daffaya <daffapasya21@gmail.com>
2023-06-04 22:11:41 +0700

    Adding Functionality on Settings > UI Mode. But still error on updating the icon. need to click it twice.

--- app/src/main/java/com/codegeniuses/estetikin/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/setting/SettingFragment.kt
@@ -1,12 +1,14 @@
 package com.codegeniuses.estetikin.ui.setting
 
 import android.app.Dialog
+import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.codegeniuses.estetikin.R
@@ -24,15 +26,33 @@ class SettingFragment : Fragment() {
     ): View {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
-        val language = binding.languageItem
-        language.setOnClickListener{
-            showBottomSheet()
+        val sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val languageCode = sharedPreferences.getString("language", "en") // Default to English if not found
+
+        // Update the text of tv_title_language based on the saved language
+        val languageTextResId = if (languageCode == "en") {
+            R.string.english // English
+        } else {
+            R.string.bahasa_indonesia // Bahasa Indonesia
         }
+        binding.tvLanguageSelected.text = getString(languageTextResId)
+
+        val language = binding.languageItem
+        language.setOnClickListener {
+            showLanguageBottomSheet()
+        }
+
+        val uiMode = binding.uiItem
+        uiMode.setOnClickListener {
+            showUiBottomSheet()
+        }
+
+
 
         return binding.root
     }
 
-    private fun showBottomSheet() {
+    private fun showLanguageBottomSheet() {
         val dialog = Dialog(requireContext())
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.item_bottom_language)
@@ -42,6 +62,7 @@ class SettingFragment : Fragment() {
 
         english.setOnClickListener {
             changeLanguage("en")
+            binding.tvLanguageSelected
             dialog.dismiss()
             Toast.makeText(requireContext(), "English is Clicked", Toast.LENGTH_SHORT).show()
         }
@@ -49,7 +70,8 @@ class SettingFragment : Fragment() {
         indonesian.setOnClickListener {
             changeLanguage("id")
             dialog.dismiss()
-            Toast.makeText(requireContext(), "Bahasa Indonesia is Clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Bahasa Indonesia is Clicked", Toast.LENGTH_SHORT)
+                .show()
         }
 
         dialog.show()
@@ -61,11 +83,63 @@ class SettingFragment : Fragment() {
         }
     }
 
+    private fun showUiBottomSheet() {
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.item_ui_mode)
+
+        dialog.show()
+
+        val light = dialog.findViewById<TextView>(R.id.light_mode)
+        val dark = dialog.findViewById<TextView>(R.id.dark_mode)
+
+
+        light.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding.ivUi.setImageResource(R.drawable.ic_light_mode)
+            binding.ivLanguage.setImageResource(R.drawable.ic_language)
+            binding.ivLogout.setImageResource(R.drawable.ic_logout)
+            dialog.dismiss()
+            Toast.makeText(requireContext(), "Light Mode is Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        dark.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            binding.ivUi.setImageResource(R.drawable.ic_dark_mode)
+            binding.ivLanguage.setImageResource(R.drawable.ic_language_light)
+            binding.ivLogout.setImageResource(R.drawable.ic_logout_light)
+            dialog.dismiss()
+            Toast.makeText(requireContext(), "Dark Mode is Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        dialog.window?.apply {
+            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            attributes?.windowAnimations = R.style.Bottom_Sheet_Animation
+            setGravity(Gravity.BOTTOM)
+        }
+    }
+
+
     private fun changeLanguage(languageCode: String) {
+        val sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("language", languageCode)
+        editor.apply()
+
         val locale = Locale(languageCode)
         val configuration = resources.configuration
         configuration.setLocale(locale)
         resources.updateConfiguration(configuration, resources.displayMetrics)
+
+        // Update the text of tv_title_language based on the selected language
+        val languageTextResId = if (languageCode == "en") {
+            R.string.english // English
+        } else {
+            R.string.bahasa_indonesia // Bahasa Indonesia
+        }
+        binding.tvLanguageSelected.text = getString(languageTextResId)
+
         requireActivity().recreate()
     }
 }
