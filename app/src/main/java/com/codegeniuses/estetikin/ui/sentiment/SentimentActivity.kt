79523dc544bb3978121cdacf5c91e9d1388b0a36
daffaya <daffapasya21@gmail.com>
2023-06-07 21:27:23 +0700

    Adding SentimentActivity.kt

---
+++ app/src/main/java/com/codegeniuses/estetikin/ui/sentiment/SentimentActivity.kt
@@ -0,0 +1,12 @@
+package com.codegeniuses.estetikin.ui.sentiment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codegeniuses.estetikin.R
+
+class SentimentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sentiment)
+    }
+}
