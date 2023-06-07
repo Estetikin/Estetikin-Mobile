4eaf6724b8f4b099c0b0ec39d2e31d53c19d5e13
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2023-06-07 16:05:52 +0700

    fixing import

--- app/src/main/java/com/codegeniuses/estetikin/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/article/ArticleFragment.kt
@@ -3,10 +3,12 @@ package com.codegeniuses.estetikin.ui.article
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
