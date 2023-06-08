f5d5399902be44c3147b543c3d3f8eefecb66c49
daffaya <daffapasya21@gmail.com>
2023-06-08 23:30:10 +0700

    Create Confirm Page before uploading picture to analyze

---
+++ app/src/main/java/com/codegeniuses/estetikin/ui/confirmPage/ConfirmFragment.kt
@@ -0,0 +1,21 @@
+package com.codegeniuses.estetikin.ui.confirmPage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codegeniuses.estetikin.R
+
+
+class ConfirmFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirm, container, false)
+    }
+
+}
