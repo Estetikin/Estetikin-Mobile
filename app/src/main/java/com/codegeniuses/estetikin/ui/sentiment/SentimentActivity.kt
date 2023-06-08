0c3c2bf963bcc79f94915d40f52d71063590f7d1
daffaya <daffapasya21@gmail.com>
2023-06-08 22:49:40 +0700

    Add listener to every button

--- app/src/main/java/com/codegeniuses/estetikin/ui/sentiment/SentimentActivity.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/sentiment/SentimentActivity.kt
@@ -1,12 +1,40 @@
 package com.codegeniuses.estetikin.ui.sentiment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.codegeniuses.estetikin.R
+import androidx.appcompat.app.AppCompatActivity
+import com.codegeniuses.estetikin.databinding.ActivitySentimentBinding
+import com.codegeniuses.estetikin.ui.MainActivity
 
 class SentimentActivity : AppCompatActivity() {
+    private var _binding: ActivitySentimentBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sentiment)
+        setContentView(binding.root)
+
+        binding.btnIos.setOnClickListener{
+//            TODO(Call the user pref fun here)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnAndroid.setOnClickListener {
+//            TODO(Call the user pref fun here)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnDslr.setOnClickListener {
+//            TODO(Call the user pref fun here)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnVideo.setOnClickListener {
+//            TODO(Call the user pref fun here)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
