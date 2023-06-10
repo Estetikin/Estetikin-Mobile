b5161d6f212340e6317f9efd1faae265783d3177
Stragel Alandia Sinaga <igelsinaga@gmail.com>
2023-06-10 12:42:25 +0700

    nambahin function ml di camera

--- app/src/main/java/com/codegeniuses/estetikin/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/camera/CameraActivity.kt
@@ -4,10 +4,12 @@ import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Intent
 import android.database.ContentObserver
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.net.Uri
 import android.os.*
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -25,11 +27,21 @@ import com.codegeniuses.estetikin.utils.createFile
 import com.codegeniuses.estetikin.utils.uriToFile
 import java.util.*
 
+// Machine Learning Deploy
+import com.codegeniuses.estetikin.ml.Model1
+import com.codegeniuses.estetikin.ml.Model2
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
 
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private var imageCapture: ImageCapture? = null
+    var imageSize = 224
 
     private var gridOverlayContainer: FrameLayout? = null
     private var galleryIcon: ImageView? = null
@@ -107,6 +119,103 @@ class CameraActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
     }
 
+    private fun classifyImage(image: Bitmap) {
+        try {
+            val model1 = Model1.newInstance(applicationContext)
+            val model2 = Model2.newInstance(applicationContext)
+
+            // Creates inputs for reference.
+            val inputFeature0: TensorBuffer =
+                TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+            val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+            byteBuffer.order(ByteOrder.nativeOrder())
+
+            // get 1D array of 224 * 224 pixels in image
+            val intValues = IntArray(imageSize * imageSize)
+            image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            var pixel = 0
+            for (i in 0 until imageSize) {
+                for (j in 0 until imageSize) {
+                    val `val` = intValues[pixel++] // RGB
+                    byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                    byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer)
+
+            // Runs model inference and gets result.
+            // Model 1
+            val outputs1: Model1.Outputs = model1.process(inputFeature0)
+            val outputFeature1: TensorBuffer = outputs1.getOutputFeature0AsTensorBuffer()
+            val confidences1: FloatArray = outputFeature1.getFloatArray()
+
+            // find the index of the class with the biggest confidence.
+            var maxPos1 = 0
+            var maxConfidence1 = 0f
+            for (i in confidences1.indices) {
+                if (confidences1[i] > maxConfidence1) {
+                    maxConfidence1 = confidences1[i]
+                    maxPos1 = i
+                }
+            }
+
+            //make the feature output data
+            val classes1 = arrayOf("well-focused picture", "blurry picture")
+            Toast.makeText(
+                applicationContext,
+                classes1[maxPos1],
+                Toast.LENGTH_SHORT
+            ).show()
+            Log.d("success", classes1[maxPos1])
+            var s1 = ""
+            for (i in classes1.indices) {
+                s1 += String.format("%s: %.1f%%\n", classes1[i], confidences1[i] * 100)
+            }
+            Toast.makeText(applicationContext, s1, Toast.LENGTH_SHORT).show()
+            Log.d("success", s1)
+
+            // Model 2
+            val outputs2: Model2.Outputs = model2.process(inputFeature0)
+            val outputFeature2: TensorBuffer = outputs2.getOutputFeature0AsTensorBuffer()
+            val confidences2: FloatArray = outputFeature2.getFloatArray()
+
+            // find the index of the class with the biggest confidence.
+            var maxPos2 = 0
+            var maxConfidence2 = 0f
+            for (i in confidences2.indices) {
+                if (confidences2[i] > maxConfidence2) {
+                    maxConfidence2 = confidences2[i]
+                    maxPos2 = i
+                }
+            }
+
+            //make the feature output data
+            val classes2 = arrayOf("food and drinks", "indoor", "outdoor", "modeling")
+            Toast.makeText(
+                applicationContext,
+                classes2[maxPos2],
+                Toast.LENGTH_SHORT
+            ).show()
+            Log.d("success", classes2[maxPos2])
+            var s2 = ""
+            for (i in classes2.indices) {
+                s2 += String.format("%s: %.1f%%\n", classes2[i], confidences2[i] * 100)
+            }
+            Toast.makeText(applicationContext, s2, Toast.LENGTH_SHORT).show()
+            Log.d("success", s2)
+
+            // Releases model resources if no longer used.
+            model1.close()
+            model2.close()
+        } catch (e: IOException) {
+            // TODO Handle the exception
+        }
+    }
+
+
     private fun takePhoto() {
         val imageCapture = imageCapture ?: return
 
@@ -128,6 +237,9 @@ class CameraActivity : AppCompatActivity() {
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
                     saveImageToGallery(savedUri)
+
+                    
+
                     val intent = Intent()
                     intent.putExtra("picture", photoFile)
                     intent.putExtra(
