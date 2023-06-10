740185714f7b12b0d424d7f77daf437c0442e158
daffaya <daffapasya21@gmail.com>
2023-06-11 00:38:53 +0700

    Create New Password Layout

---
+++ app/src/main/java/com/codegeniuses/estetikin/ui/forgetPassword/CreatePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.codegeniuses.estetikin.ui.forgetPassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codegeniuses.estetikin.R
+
+class CreatePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_password)
+    }
+}
