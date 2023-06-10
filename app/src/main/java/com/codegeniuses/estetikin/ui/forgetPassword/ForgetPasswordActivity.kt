ff0877a1c2983961ead99272d2d983bb78674fe9
daffaya <daffapasya21@gmail.com>
2023-06-11 00:10:51 +0700

    Create Forget Password Activity

--- app/src/main/java/com/codegeniuses/estetikin/ui/forgetPassword/ForgetPasswordActivity.kt
+++ app/src/main/java/com/codegeniuses/estetikin/ui/forgetPassword/ForgetPasswordActivity.kt
@@ -2,6 +2,7 @@ package com.codegeniuses.estetikin.ui.forgetPassword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codegeniuses.estetikin.R
 
 class ForgetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
