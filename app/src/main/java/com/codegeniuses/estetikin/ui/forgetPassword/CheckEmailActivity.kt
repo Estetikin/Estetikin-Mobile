1dd578fb884172da411e9fc6a02b22b9a1344403
daffaya <daffapasya21@gmail.com>
2023-06-11 09:57:56 +0700

    Finishing the Forget Password, Check Email, and Create New Password layout

---
+++ app/src/main/java/com/codegeniuses/estetikin/ui/forgetPassword/CheckEmailActivity.kt
@@ -0,0 +1,12 @@
+package com.codegeniuses.estetikin.ui.forgetPassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codegeniuses.estetikin.R
+
+class CheckEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_email)
+    }
+}
