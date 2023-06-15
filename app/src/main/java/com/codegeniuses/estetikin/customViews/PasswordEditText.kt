9008fd498bbf41659b8a9e1ac041241af4ea69e2
daffaya <daffapasya21@gmail.com>
2023-06-15 12:43:39 +0700

    change the approach of custom view for password, but still cannot change the icon

--- app/src/main/java/com/codegeniuses/estetikin/customViews/PasswordEditText.kt
+++ app/src/main/java/com/codegeniuses/estetikin/customViews/PasswordEditText.kt
@@ -2,7 +2,9 @@ package com.codegeniuses.estetikin.customViews
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
@@ -44,6 +46,16 @@ class PasswordEditText : AppCompatEditText {
 
         updateCompoundDrawables()
 
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateError()
+                updateCompoundDrawables()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
         setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_UP && event.rawX >= (right - (compoundPaddingEnd + eyeOffDrawable.intrinsicWidth))) {
                 togglePasswordVisibility()
@@ -75,9 +87,10 @@ class PasswordEditText : AppCompatEditText {
         updateCompoundDrawables()
     }
 
-    override fun onTextChanged(text: CharSequence?, start: Int, lengthBefore: Int, lengthAfter: Int) {
-        super.onTextChanged(text, start, lengthBefore, lengthAfter)
-        updateCompoundDrawables()
+    private fun updateError() {
+        val password = text?.toString() ?: ""
+        val error = if (password.length >= 6) null else "Password should be at least 6 characters long."
+        this.error = error
     }
 
     override fun performClick(): Boolean {
